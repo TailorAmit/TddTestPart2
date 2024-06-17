d6936a91fa1681b6f0b5cacc4a5f34805fe5c7ad
TailarAmit <amit.tailor@etevatech.com>
2024-06-17 20:43:42 +0530

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <h1>Tdd Demo</h1>
+    </div>
+  );
+}
+
+export default App;
