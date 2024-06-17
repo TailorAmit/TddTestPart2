51c7182399d00fc7a370a07ec9958b9a5117404a
TailarAmit <amit.tailor@etevatech.com>
2024-06-17 20:51:26 +0530

    add test case for returns the number itself for a single number

--- src/const/const.test.ts
+++ src/const/const.test.ts
@@ -1,6 +1,9 @@
 import { add } from './const';
 
 test('returns 0 for an empty string', () => {
-    expect(add('')).toBe(0);
-  });
-   
+  expect(add('')).toBe(0);
+});
+
+test('returns the number itself for a single number', () => {
+  expect(add('1')).toBe(1);
+});
