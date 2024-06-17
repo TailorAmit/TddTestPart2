40509b17c925077e9e6352941a43a0d77cf66306
TailarAmit <amit.tailor@etevatech.com>
2024-06-17 20:46:32 +0530

    add new function calculation in const directry

---
+++ src/const/const.ts
@@ -0,0 +1,26 @@
+// add function
+export const calculation = (numbers: string): number | undefined => {
+    // check argument is null or not 
+    if (!numbers) {
+        if (!numbers) {
+            return 0;
+        }
+
+        let delimiter = ',';
+        if (numbers.startsWith('//')) {
+            const parts = numbers.split('\n');
+            delimiter = parts[0][2];
+            numbers = parts[1];
+        }
+
+        const numberList: number[] = numbers.split(new RegExp(`[${delimiter}\n]`)).map(Number);
+        const filteredNumbers: number[] = numberList.filter(num => num <= 1000);
+
+        const negatives: number[] = filteredNumbers.filter(num => num < 0);
+        if (negatives.length > 0) {
+            throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
+        }
+
+        return filteredNumbers.reduce((sum, num) => sum + num, 0);
+    }
+}
